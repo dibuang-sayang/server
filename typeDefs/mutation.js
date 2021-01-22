35ee53b865a36d77fd03a91ea28e334de6303546
tiorzal <tioralmunazir@gmail.com>
2021-01-22 21:49:33 +0700

    implement login register

--- typeDefs/mutation.js
+++ typeDefs/mutation.js
@@ -3,16 +3,16 @@ const { gql } = require("apollo-server");
 module.exports = gql`
   type Mutation {
     register(data: UserData): User
-    deleteUser(id: ID!) : Message
-    editUser(id: ID!, data : UserData) : User
+    deleteUser(id: ID!): Message
+    editUser(id: ID!, data: UserData): User
+    loginUser(email: String, password: String): Token
 
-    addOffice(data : OfficeData) : Office
-    deleteOffice(id : ID!) : OfficeMessage
-    editOffice(id : ID!, data :OfficeData ) : Office
+    addOffice(data: OfficeData): Office
+    deleteOffice(id: ID!): OfficeMessage
+    editOffice(id: ID!, data: OfficeData): Office
 
-    addProduct(data : ProductData) : Product
-    deleteProduct(id : ID!): Message
-    editProduct(id : ID!, data : ProductData) : Product
-
+    addProduct(data: ProductData): Product
+    deleteProduct(id: ID!): Message
+    editProduct(id: ID!, data: ProductData): Product
   }
 `;
