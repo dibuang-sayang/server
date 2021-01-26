34b139afae99bb90997169003016673faba4dfd1
tiorzal <tioralmunazir@gmail.com>
2021-01-26 21:52:22 +0700

    update

--- resolvers/office/getOfficeById.js
+++ resolvers/office/getOfficeById.js
@@ -4,11 +4,13 @@ const { authentication } = require("../../helpers/authentication");
 module.exports = authentication(async (_, args, { user , models}) => {
   try {
     const {Office} = models
-    const officeData = Office.findOne({
+    const officeData = await Office.findOne({
       where: {
         UserId: user.id,
       },
+      include: [models.Product]
     });
+    console.log(officeData);
     return officeData;
   } catch (error) {
     return error;
